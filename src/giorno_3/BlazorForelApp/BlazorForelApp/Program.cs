284c68bfbe7b96df370fdef8b142f7f3ea297969
alessio iafrate <a.iafrate@gmail.com>
2025-10-22 13:01:27 +0200

    traduzioni

--- src/giorno_3/BlazorForelApp/BlazorForelApp/Program.cs
+++ src/giorno_3/BlazorForelApp/BlazorForelApp/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
 
+using System.Globalization;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -14,5 +16,7 @@ builder.Services.AddBlazoredLocalStorage();
 
 // Aggiunge Blazor.Bootstrap
 builder.Services.AddBlazorBootstrap();
+
+builder.Services.AddLocalization();
 
 await builder.Build().RunAsync();
